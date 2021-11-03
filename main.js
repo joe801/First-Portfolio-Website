7a340fea070ad936d05ae7f7644b720ed020dca0
joecrypt <oyakhilomejoseph12@gmail.com>
2021-11-03 20:23:04 +0100

    My first portfolio, ready for computer view

---
+++ main.js
@@ -0,0 +1,25 @@
+// Smooth Scrolling //
+$("#navbar a, .btn").on("click", function(event) {
+	if(this.hash !== '') {
+		event.preventDefault();
+
+		const hash = this.hash;
+
+	$("html, body").animate(
+		{
+		scrollTop: $(hash).offset().top - 30
+		},
+		800
+		);
+	}
+});
+
+$(document).ready(function(){
+
+	// toggle menu/navbar script //
+
+	$('.bars-btn').click(function(){
+		$('.navbar .menu').toggleClass("active");
+		$('.bars-btn i').toggleClass("active");
+	});
+});
