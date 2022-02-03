9561ac1b406a4d0db0eb7386c09f7a9253ca737e
joecrypt <oyakhilomejoseph12@gmail.com>
2022-02-03 22:53:56 +0100

    Added project page

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 // Smooth Scrolling //
-$("#navbar a, .btn").on("click", function(event) {
-	if(this.hash !== '') {
-		event.preventDefault();
+	$("#navbar a, .btn").on("click", function(event) {
+		if(this.hash !== '') {
+			event.preventDefault();
 
-		const hash = this.hash;
+			const hash = this.hash;
 
-	$("html, body").animate(
-		{
-		scrollTop: $(hash).offset().top - 30
-		},
-		800
-		);
-	}
-});
+		$("html, body").animate(
+			{
+			scrollTop: $(hash).offset().top - 30
+			},
+			800
+			);
+		}
+	});	
 
 $(document).ready(function(){
 
---
+++ projectPage.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	// toggle menu/navbar script //
+
+	$('.bars-btn').click(function(){
+		$('.navbar .menu').toggleClass("active");
+		$('.bars-btn i').toggleClass("active");
+	});
+});
