07a5f59059df27de4ef54caee978986a0dcae349
joecrypt <oyakhilomejoseph12@gmail.com>
2022-02-13 23:46:29 +0100

    added third project

--- main.js
+++ main.js
@@ -24,3 +24,23 @@ $(document).ready(function(){
 	});
 });
 
+var icon = document.getElementById("moon");
+var icon2 = document.getElementById("sun");
+
+icon.onclick = function(){
+	document.body.classList.add("dark-theme");
+	document.body.classList.remove("body")
+	if (document.body.classList.contains("dark-theme")) {
+		icon.style.display = "none";
+		icon2.style.display = "inline-block";
+	}
+}
+
+icon2.onclick = function(){
+	document.body.classList.add("body")
+	document.body.classList.remove("dark-theme");
+	if (document.body.classList.contains("body")) {
+		icon.style.display = "inline-block";
+		icon2.style.display = "none";
+	}
+}
--- projectPage.js
+++ projectPage.js
@@ -7,3 +7,24 @@ $(document).ready(function(){
 		$('.bars-btn i').toggleClass("active");
 	});
 });
+
+var icon = document.getElementById("moon");
+var icon2 = document.getElementById("sun");
+
+icon.onclick = function(){
+	document.body.classList.add("dark-theme");
+	document.body.classList.remove("body")
+	if (document.body.classList.contains("dark-theme")) {
+		icon.style.display = "none";
+		icon2.style.display = "inline-block";
+	}
+}
+
+icon2.onclick = function(){
+	document.body.classList.add("body")
+	document.body.classList.remove("dark-theme");
+	if (document.body.classList.contains("body")) {
+		icon.style.display = "inline-block";
+		icon2.style.display = "none";
+	}
+}
